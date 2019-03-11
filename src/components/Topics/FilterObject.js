2233d4ec6a3898fe397ccca849f1aefc4bdb7426
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-11 16:44:05 -0600

    finish all steps

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 
-class FilterObject extends Component {
-    render () {
+export default class FilterObject extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            countries: [
+                {
+                    name: 'Brazil',
+                    size: 'big',
+                    populationInMillions: 200,
+                },
+                {
+                    name: 'Turkey',
+                    populationInMillions: 80,
+                    religion: 'Islam',
+                },
+                {
+                    name: 'Nicaragua',
+                    size: 'small',
+                }
+            ],
+
+            userInput: '',
+            filteredCountries: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+
+    filterCountries(prop) {
+        var countries = this.state.countries;
+        var newList = [];
+
+        for (var i = 0; i < countries.length; i++) {
+            if (countries[i].hasOwnProperty(prop)) {
+                newList.push(countries[i]);
+            }
+        }
+        this.setState({ filteredCountries: newList });
+    }
+
+    render() {
         return (
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">Original List: { JSON.stringify(this.state.countries, null, 10) } </span>
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+                <button className="confirmationButton" onClick={ () => this.filterCountries(this.state.userInput) }>Filter</button>
+                <span className="resultsBox filterObjectRB">Filtered List: { JSON.stringify(this.state.filteredCountries, null, 10) } </span>
+            </div>
         )
     }
 }
-
-export default FilterObject
