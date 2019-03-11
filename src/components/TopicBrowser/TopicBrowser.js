7c60f3b0ecc4bde0892183ca4b8317844ed7d218
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-11 14:01:38 -0600

    step2

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+import EvenAndOff from '../Topics/EvenAndOdd'
+import FilterObject from '../Topics/FilterObject'
+import FilterString from '../Topics/FilterString'
+import Palindrome from '../Topics/Palindrome'
+import Sum from '../Topics/Sum'
+
+class TopicBrowser extends Component {
+    render() {
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
+
+export default TopicBrowser;
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react'
+
+class FilterObject extends Component {
+    render () {
+        return (
+            <p>FilterObject Component</p>
+        )
+    }
+}
+
+export default FilterObject
